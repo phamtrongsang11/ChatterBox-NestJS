89183a9b6bad16af29928e6c6291f4c952685ee5
Sang Pham Trong <phamtrongsang11@gmail.com>
2024-03-01 17:00:07 +0700

    enable core for another url

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173','https://chatterboxs-react.netlify.app'],
     allowedHeaders: [
       'Accept',
       'Authorization',
