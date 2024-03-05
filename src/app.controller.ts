827f458f304b61dbdeb1099d47515bd56099382b
Sang Pham Trong <phamtrongsang11@gmail.com>
2024-03-05 11:30:21 +0700

    add livekit controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+
+import { LivekitService } from './livekit/livekit.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly livekitService: LivekitService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/livekit')
+  async getLiveKitToken(
+    @Query('identity') identity: string,
+    @Query('chatId') chatId: string,
+  ) {
+    return await this.livekitService.createAccessToken(identity, chatId);
   }
 }
